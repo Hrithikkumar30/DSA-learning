c25648c31eae8c84022867445017b255d73283b1
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2023-07-22 16:03:39 +0530

    Inserting at the end

--- linked-list.py.py
+++ linked-list.py.py
@@ -37,17 +37,56 @@ class LinkedList:
         while curr!=None:
             print(curr.data)
             curr=curr.next
+            
+    # 2.Inserting at the end
+    
+    def insert_at_end(self,value):
         
-LL= LinkedList()
+        new_node = Node(value)
+        #resolving empty list problem/error
+        if self.head==None:
+            self.head=new_node
+            self.n=self.n+1
+            return
+            
+        curr = self.head
+        
+        while curr.next!=None:
+            curr=curr.next
+        
+        #now loop will end then curr is at last node
+        curr.next=new_node #assigned last node as new node
+        self.n= self.n+1
+        
+        
+    def insert_after(self,after,value):
+        new_node = Node(value)
+        curr = self.head
+        
+        while curr!=None:
+            if curr.data==after:
+                break
+            curr=curr.next
+            
+            # Here we have two cases
+            # 1. if condition breaks it means item found in the linked list -> curr -> not none
+        if curr != None:
+            new_node.next=curr.next
+            curr.next=new_node
+        else:
+            print( 'item not found') # 2. if condition does not breaks means loop pura chala-> item does not found in the linked List ->curr->none
 
+            
+LL= LinkedList()
 LL.insert_head(1)
 LL.insert_head(2)
 LL.insert_head(3)
 LL.insert_head(4)
+LL.insert_at_end(5)
+LL.insert_after(5,40)
 
 print(LL.traverse())  
 
-print(len(LL))
         
         
         
