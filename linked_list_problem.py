3701c98b0942cccf6788d0eb72765a6c62ebe590
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2023-07-23 12:48:41 +0530

    add item in the linked List present at  odd index

--- linked_list_problem.py
+++ linked_list_problem.py
@@ -14,3 +14,21 @@ linked list
     #         temp=temp.next
     #     max.data=value
 #  LL.replace_max(21)
+
+
+"""
+Write a program to add item in the linked List present at the odd index
+"""
+
+    # def sum_of_odd(self):
+    #     curr = self.head
+    #     pos=0
+    #     result = 0
+        
+    #     while curr != None:
+    #         if pos%2 !=0:
+    #             result = result+curr.data
+    #         pos+=1
+    #         curr=curr.next
+    #     print(result)
+                
