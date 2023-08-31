ceb31b7914187138ff507137ac289c44a68611f4
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2023-08-31 16:23:06 +0530

    Delete the Middle Node of a Linked List

--- linked_list_problem.py
+++ linked_list_problem.py
@@ -134,3 +134,21 @@ Maximum Twin Sum of a Linked List
 #             s = s.next
 #             prev =prev.next
 #         return res
+
+"""Delete the Middle Node of a Linked List
+"""
+
+    # def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     if not head.next : return None
+    #     slow = head
+    #     fast =head
+    #     prev = head 
+
+    #     while fast and fast.next !=None:
+    #         prev = slow
+    #         slow = slow.next
+    #         fast = fast.next.next
+            
+
+    #     prev.next = slow.next
+    #     return head
